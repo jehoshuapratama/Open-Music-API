42ad29671bc9d641a9cf222633c58ff64e3af4dc
Jehoshua Pratama <50482705+jehoshuapratama@users.noreply.github.com>
2022-09-19 15:00:23 +0800

    initial commit

---
+++ src/services/postgres/SongsService.js
@@ -0,0 +1,87 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+/* eslint linebreak-style: ["error", "windows"] */
+
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const { mapDBToModelSong } = require('../../utils');
+const NotFoundError = require('../../exceptions/NotFoundError');
+
+class SongsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addSong({
+    title, year, genre, performer, duration, album_id,
+  }) {
+    const id = `song-${nanoid(16)}`;
+    const createdAt = new Date(0).toISOString();
+    const updateAt = createdAt;
+
+    const query = {
+      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id',
+      values: [id, title, year, genre, performer, duration, album_id, createdAt, updateAt],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Lagu gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getSongs() {
+    const result = await this._pool.query('SELECT id, title, performer FROM songs');
+    return result.rows.map(mapDBToModelSong);
+  }
+
+  async getSongById(id) {
+    const query = {
+      text: 'SELECT * FROM songs WHERE id = $1',
+      values: [id],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Lagu tidak ditemukan');
+    }
+
+    return result.rows.map(mapDBToModelSong)[0];
+  }
+
+  async editSongById(id, {
+    title, year, genre, performer, duration, album_id,
+  }) {
+    const updatedAt = new Date().toISOString();
+    const query = {
+      text: 'UPDATE songs SET title = $1, year = $2, genre = $3, performer = $4, duration = $5, album_id = $6, updated_at = $7 WHERE id = $8 RETURNING id',
+      values: [title, year, genre, performer, duration, album_id, updatedAt, id],
+    };
+    const result = await this._pool.query(query);
+    console.log('Test');
+    console.log(result);
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbarui lagu. Id tidak ditemukan');
+    }
+  }
+
+  async deleteSongById(id) {
+    const query = {
+      text: 'DELETE FROM songs WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Lagu gagal dihapus. Id tidak ditemukan');
+    }
+  }
+}
+
+module.exports = SongsService;
---
+++ src/validator/songs/schema.js
@@ -0,0 +1,14 @@
+/* eslint linebreak-style: ["error", "windows"] */
+
+const Joi = require('joi');
+
+const SongPayloadSchema = Joi.object({
+  title: Joi.string().required(),
+  year: Joi.number().required(),
+  genre: Joi.string().required(),
+  performer: Joi.string().required(),
+  duration: Joi.number(),
+  album_id: Joi.string(),
+});
+
+module.exports = { SongPayloadSchema };
